cf8cae94149aafe2f0c6cef42e044c6a131b3b90
Sebastian Florian <rebardesigndk@gmail.com>
2015-11-30 16:35:18 +0100

    feature (localizaApp) generate yo angular app

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name localizeAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the localizeAppApp
+ */
+angular.module('localizeAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
